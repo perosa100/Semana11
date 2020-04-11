0bd1200c38f72c046bf692a6d4b73941bb7abd90
Patrick Perosa <patrickperosapp@gmail.com>
2020-04-10 22:24:10 -0400

    backend da aplicacao

---
+++ backend/src/routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const OngControler = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/session', SessionController.create);
+
+routes.get('/ongs', OngControler.index);
+routes.post('/ongs', OngControler.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;
